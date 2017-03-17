25076cbe82eb3d6c0522e61275275f7b42a05777
Hariharan <hariharantex166@gmail.com>
2017-03-17 16:12:20 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Page</title>
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+  <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>.
+  <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/image_Scroll.css">
+<table>
+<body background="univ2.jpg" style="background-repeat: no-repeat">
+</table>
+<ul class="w3-navbar w3-green">
+  <div class="container-fluid">
+    <div class="navbar-header">
+     <h4 style="color:white;"><span class="glyphicon glyphicon-earphone"></span> &nbsp; 0288-261199</h4> 
+        </div>
+    <ul class="nav navbar-nav">
+      <li> <h4 style="color:white;">&nbsp;&nbsp;<span class="glyphicon glyphicon-comment"></span> &nbsp; Contact Us</h4> </li>
+       <li> <h4 style="color:white;">&nbsp;&nbsp;<span class="glyphicon glyphicon-log-in"></span> &nbsp; Sign In</h4> </li>
+        <li> <h4 style="color:white;">&nbsp;&nbsp;<span class="glyphicon glyphicon-edit"></span> &nbsp; Create Account</h4> </li>
+        </ul>
+        <ul class="nav navbar-nav pull-right">
+        <li> <h4 style="color:white;text-align:right;">Follow Us:</h4> </li>&nbsp;&nbsp;
+        <img src="t.png">&nbsp;&nbsp;
+         <img src="f.png">&nbsp;&nbsp;</div>
+</nav><br>
+  <div class="container">
+  <div class="pull-left">
+    <table style="width:50%">
+    <tr>  
+      <td><img style="width: 200px;height: 110px" src="lo.png"></td>
+      </tr>
+      </table>
+      </div>
+      <div class="pull-right">
+    <table style="width:50%">
+    <tr>  
+     <td>
+     <form>
+  <div class="input-group">
+  <input id="email" type="text" class="form-control" name="email" placeholder="Search">
+    <span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>
+    </div>
+    </form>
+        </td>
+         <div class="pull-right">
+    <table style="width:50%"><br>
+    <tr>  
+     <td><img src="t.png">&nbsp;
+     <img src="f.png">&nbsp;
+     <img src="w.png"> 
+     <li style="color:white;text-align:right;">Share</li>
+     </td>
+     </tr>
+     </table></div>
+     </tr>
+     </table>
+     </div></div>
+     </ul>
+     </div> </ul> 
+
+    </table>
+    </table>
+  </div>
+</table>
+</body>
+</table><br>
+<ul class="w3-navbar w3-green">
+  <div class="container-fluid">
+    <div class="navbar-header">
+       <h4 style="color:white;"> Metroite for sale>></h4> &nbsp;&nbsp;
+        </div>
+    <ul class="nav navbar-nav">
+      <li> <h4 style="color:white;"> &nbsp;&nbsp;Impacities tacticies for sale>></h4> </li>
+       < <li> <h4 style="color:white;"> &nbsp;&nbsp;Other items>></h4> </li>
+         <li> <h4 style="color:white;"> &nbsp;&nbsp;Spot light>></h4> </li>
+          <li> <h4 style="color:white;">  &nbsp;&nbsp;Gift Certificate>></h4> </li>
+           <li> <h4 style="color:white;"> &nbsp;&nbsp; Client Services>></h4> </li>
+            <li> <h4 style="color:white;"> &nbsp;&nbsp; Education and research>></h4> </li>
+        </ul><br>
+        <div style="margin-top: 300px;">
+        <!-- <img src="images/s1.jpg"> -->
+        <header id="myCarousel" class="carousel slide">
+          <ol class="carousel-indicators">
+            <h1><span class="label label-default">Powered By X-Cart reBOOT</span></h1><br>
+            <h2><span class="label label-default">The Bootstrap based,fully responsive template</span></h2><br>
+            <button class="btn btn-success form-control" id="btn_purchase">Purchase</button><br>
+              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+              <li data-target="#myCarousel" data-slide-to="1"></li>
+              <li data-target="#myCarousel" data-slide-to="2"></li>
+         </ol> 
+    <div class="carousel-inner">
+        <div class="item active">
+           <div class="fill" style=" background-image:url('images/s1.jpg');"></div>
+        </div>
+        <div class="item">
+            <div class="fill" style="background-image:url('images/s2.jpg');"></div>
+        </div>
+        <div class="item">
+            <div class="fill" style="background-image:url('images/s3.jpg');"></div>
+        </div>
+    </div>
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+</header>
+    <script>
+    $('.carousel').carousel({
+        interval: 5000
+    })
+    </script>
+    </div>
+</head>
+</html>
+
+
+
+</body>
+</html>
